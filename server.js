777f154a0753b6e3491643c167a4e77d47b170e4
gloryguitar <gloryguitar05@gmail.com>
2026-09-01 15:15:24 +0700

    feat: assignment6

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db');
+//Routes files
+const hospitals = require('./routes/hospitals');
+const auth = require('./routes/auth');
+
+//Load env vars
+dotenv.config({path:'./config/config.env'});
+
+// Connect to database
+connectDB();
+
+const app=express();
+
+// Body parser
+app.use (express.json());
+
+// Cookie parser
+app.use(cookieParser());
+
+// Mount routers
+app.use('/api/v1/hospitals', hospitals);
+app.use('/api/v1/auth',auth);
+
+const PORT=process.env.PORT || 5000;
+const server = app.listen (PORT,console.log('Server running in ', process.env.NODE_ENV,' mode on port ', PORT));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection',(err,promise)=>{
+    console.log(`Error: ${err.message}`);
+    server.close(()=>{
+        process.exit(1);
+    });
+});
